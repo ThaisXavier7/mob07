3ae3a6ed386d33433fafb1529ccb1d95bfa4709d
ThaisXavier7 <xavierthais627@gmail.com>
2026-09-25 17:04:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    document.getElementById("menu").classList.toggle("aberto");
+}
+// Validação básica com required.
+// Seção estática.
+// Seção estática.
+// Seção estática.
+// Seção estática.
